f13f8c443e0664adc682680385c10e8e55bebfbf
clujin <clujin@gmail.com>
2016-01-15 23:39:32 -0800

    challenge 2 passes sometimes

--- challenge01.js
+++ challenge01.js
@@ -5,9 +5,20 @@
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", function() {
             // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
+            var length = elevator.getPressedFloors.length();
+            if (length > 0) {
+                elevator.goToFloor(elevator.getPressedFloors[length-1]);
+            }
+        });
+        
+        elevator.on("floor_button_pressed", function(floorNum) {
+            elevator.destinationQueue.push(floors[floorNum]);
+        });
+        
+        elevator.on("passing_floor", function(floorNum, direction){
+            if (elevator.destinationDirection() === direction) {
+                elevator.goToFloor(floorNum, true);
+            }
         });
     },
     update: function(dt, elevators, floors) {
